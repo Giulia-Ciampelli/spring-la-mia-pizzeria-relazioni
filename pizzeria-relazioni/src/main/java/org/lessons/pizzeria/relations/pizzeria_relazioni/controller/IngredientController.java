3a64f1b726212a0da287b9df60408404176a1a3a
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-04-15 17:51:45 +0200

    docs: add ingredients form and create section to ingredients controller

--- pizzeria-relazioni/src/main/java/org/lessons/pizzeria/relations/pizzeria_relazioni/controller/IngredientController.java
+++ pizzeria-relazioni/src/main/java/org/lessons/pizzeria/relations/pizzeria_relazioni/controller/IngredientController.java
@@ -1,11 +1,18 @@
 package org.lessons.pizzeria.relations.pizzeria_relazioni.controller;
 
+import org.lessons.pizzeria.relations.pizzeria_relazioni.model.Ingredient;
 import org.lessons.pizzeria.relations.pizzeria_relazioni.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ingredients")
@@ -19,5 +26,28 @@ public class IngredientController {
     public String index(Model model) {
         model.addAttribute("ingredients", ingredientRepository.findAll());
         return "ingredients/index";
+    }
+
+    // sezione show
+    @GetMapping("/{id}")
+    public String show(@PathVariable int id, Model model) {
+        model.addAttribute("ingredient", ingredientRepository.findById(id).get());
+        return "ingredients/show";
+    }
+
+    // sezione create
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("ingredient", new Ingredient());
+        return "ingredients/create-edit";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("ingredient") Ingredient formIngredient, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "ingredients/create-edit";
+        }
+        ingredientRepository.save(formIngredient);
+        return "redirect:/ingredients";
     }
 }
